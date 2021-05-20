88ce47074001f1fe0048f20ef5313d9750424526
Tofan Stefan <tofanstefan99@gmail.com>
2021-05-20 15:51:40 +0300

    assist club (connect to assist club and get all members)

--- src/strava-assist-club/strava-assist-club.service.ts
+++ src/strava-assist-club/strava-assist-club.service.ts
@@ -7,13 +7,10 @@ const strava = require('strava-v3')
 @Injectable()
 export class StravaAssistClubService {
   constructor(private readonly stravaAuthService: StravaAuthService){}
-  create(createStravaAssistClubDto: CreateStravaAssistClubDto) {
-    return 'This action adds a new stravaAssistClub';
-  }
 
  async  getClubInfo(strava_id : number) {
    try{
-    const club = await strava.clubs.get({id:207985,access_token:await this.stravaAuthService.getAccessToken(strava_id)})
+    const club = await strava.clubs.get({id:process.env.STRAVA_ASSIST_CLUB_ID,access_token:await this.stravaAuthService.getAccessToken(strava_id)})
     console.log(club);
     return club;
    }catch(error){
@@ -25,9 +22,10 @@ export class StravaAssistClubService {
   async getMembers(strava_id : number) {
     try{
       const members = await strava.clubs.listMembers(
-        {id:207985,
+        { id:process.env.STRAVA_ASSIST_CLUB_ID,
           per_page :200,
-          access_token:await this.stravaAuthService.getAccessToken(strava_id)})
+          access_token:await this.stravaAuthService.getAccessToken(strava_id)
+        })
       console.log(members);
       return members;
      }catch(error){
